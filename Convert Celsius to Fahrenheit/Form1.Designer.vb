ce66dafe72122806e17e43e70bacf89a3fee787f
Moolvie <Moolvie@gmail.com>
2017-02-05 10:49:07 -0500

    Add project files.

---
+++ Convert Celsius to Fahrenheit/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Fahrenheit = New System.Windows.Forms.Label()
+        Me.Celsius = New System.Windows.Forms.TextBox()
+        Me.CalcButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.StatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(34, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(111, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Degrees Celsius"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 111)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(110, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Equivalent Fahrenheit"
+        '
+        'Fahrenheit
+        '
+        Me.Fahrenheit.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Fahrenheit.Location = New System.Drawing.Point(151, 110)
+        Me.Fahrenheit.Name = "Fahrenheit"
+        Me.Fahrenheit.Size = New System.Drawing.Size(100, 23)
+        Me.Fahrenheit.TabIndex = 2
+        '
+        'Celsius
+        '
+        Me.Celsius.Location = New System.Drawing.Point(151, 20)
+        Me.Celsius.Name = "Celsius"
+        Me.Celsius.Size = New System.Drawing.Size(100, 20)
+        Me.Celsius.TabIndex = 3
+        '
+        'CalcButton
+        '
+        Me.CalcButton.Location = New System.Drawing.Point(27, 211)
+        Me.CalcButton.Name = "CalcButton"
+        Me.CalcButton.Size = New System.Drawing.Size(75, 23)
+        Me.CalcButton.TabIndex = 4
+        Me.CalcButton.Text = "Convert"
+        Me.CalcButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(108, 211)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(75, 23)
+        Me.ClearButton.TabIndex = 5
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(189, 211)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 23)
+        Me.ExitButton.TabIndex = 6
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StatusLabel})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 239)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(284, 22)
+        Me.StatusStrip1.TabIndex = 7
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'StatusLabel
+        '
+        Me.StatusLabel.Name = "StatusLabel"
+        Me.StatusLabel.Size = New System.Drawing.Size(0, 17)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.CalcButton)
+        Me.Controls.Add(Me.Celsius)
+        Me.Controls.Add(Me.Fahrenheit)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Celsius to Fahrenheit"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Fahrenheit As System.Windows.Forms.Label
+    Friend WithEvents Celsius As System.Windows.Forms.TextBox
+    Friend WithEvents CalcButton As System.Windows.Forms.Button
+    Friend WithEvents ClearButton As System.Windows.Forms.Button
+    Friend WithEvents ExitButton As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents StatusLabel As System.Windows.Forms.ToolStripStatusLabel
+
+End Class
---
+++ Convert Celsius to Fahrenheit/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        ' close the form
+        Me.Close()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        ' Clear the form and return focus to celsius textbox
+        Fahrenheit.Text = String.Empty
+        StatusLabel.Text = String.Empty
+
+        Celsius.Clear()
+
+        Celsius.Focus()
+    End Sub
+
+    Private Sub CalcButton_Click(sender As Object, e As EventArgs) Handles CalcButton.Click
+        ' Declare Variables
+        Dim TempCelsius As Double = 0
+        Dim TempFahrenheit As Double = 0
+        Try
+            TempCelsius = Convert.ToDouble(Celsius.Text)
+
+            TempFahrenheit = 1.8 * TempCelsius + 32
+            Fahrenheit.Text = Convert.ToString(TempFahrenheit)
+        Catch
+            StatusLabel.Text = "you must enter numeric data"
+        End Try
+    End Sub
+End Class
